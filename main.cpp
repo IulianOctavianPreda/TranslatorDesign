9c934700c3ff9063cb9926447c39fc7edb1ef84a
Iulian Preda <iulian.octavian.preda@gmail.com>
2019-10-22 17:51:49 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,29 +1,33 @@
-#include <iostream>
+#include <cstdlib>
 #include <cstring>
-using namespace std;
-#include <cstdlib> /* system() */
-int main(int argc, char *argv[])
-{
- switch(*argv[1])
- {
-    case '1':
-    {
-        system ("flex lexer.l");
-        break;
+#include <iostream>
+
+#include "cmm_driver.hpp"
+
+int main(const int argc, const char **argv) {
+  /** check for the right # of arguments **/
+  if (argc == 2) {
+    CMM::CMM_Driver driver;
+    /** example for piping input from terminal, i.e., using cat **/
+    if (std::strncmp(argv[1], "-o", 2) == 0) {
+      driver.parse(std::cin);
     }
-    case '2':
-    {
-        /*system ("bison -o y.y");*/
-		cout<<"bison file does not exist yet";
-        break;
+    /** simple help menu **/
+    else if (std::strncmp(argv[1], "-h", 2) == 0) {
+      std::cout << "use -o for pipe to std::cin\n";
+      std::cout << "just give a filename to count from a file\n";
+      std::cout << "use -h to get this menu\n";
+      return (EXIT_SUCCESS);
     }
-    default:
-    {
-        /*system ("flex lexer.l");
-        system ("bison -o y.y");*/
-		cout<<"bison file does not exist yet";
-        break;
+    /** example reading input from a file **/
+    else {
+      /** assume file, prod code, use stat to check **/
+      driver.parse(argv[1]);
     }
- }
- return 0;
+    driver.print(std::cout) << "\n";
+  } else {
+    /** exit with failure condition **/
+    return (EXIT_FAILURE);
+  }
+  return (EXIT_SUCCESS);
 }
