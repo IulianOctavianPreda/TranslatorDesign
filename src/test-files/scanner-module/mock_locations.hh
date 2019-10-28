23d46026af9735b8c0712ff1a9714b37eda6ee62
Iulian Preda <iulian.octavian.preda@gmail.com>
2019-10-28 22:12:54 +0200

    Added mock locations and tokens

---
+++ src/test-files/scanner-module/mock_locations.hh
@@ -0,0 +1,11 @@
+namespace CMM {
+class location{
+  public:
+    explicit location (int position){}
+
+  public:
+    void step (){}
+    void columns (int count = 1){}
+    void lines (int count = 1){}
+
+}
