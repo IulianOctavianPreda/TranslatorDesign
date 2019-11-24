396c53bba0355cd09d4ffb4622a36d155767e7e5
Iulian Preda <iulian.octavian.preda@gmail.com>
2019-11-25 01:47:45 +0200

    Added helper header file  for AST

---
+++ AST/helpers/helpers.hpp
@@ -0,0 +1,8 @@
+#include <cassert>
+#include <map>
+#include <string>
+#include <vector>
+
+int getOperation(int op, int typeLeft, int typeRight);
+std::string getTypeName(int type);
+std::string getOperationName(int op);
