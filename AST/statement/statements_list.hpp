ee1cf7509617663c3502001b703195ba9f1dfec3
Gabriela Dinu <gdinu93@yahoo.com>
2019-11-25 17:55:08 -0500

    Created statementsList

---
+++ AST/statement/statements_list.hpp
@@ -0,0 +1,14 @@
+#include <memory>
+#include <vector>
+#include "../base/base_statement.hpp"
+
+class StatementsList {
+  std::vector<std::shared_ptr<BaseStatement>> _statements;
+
+ public:
+  StatementsList* Add(std::shared_ptr<BaseStatement> statement) {
+    assert(statement);
+    _statements.push_back(statement);
+    return this;
+  }
+};
