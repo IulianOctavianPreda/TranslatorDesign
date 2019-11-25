3202e8d620e7049ddc9c72a6864d607df96d6473
Iulian Preda <iulian.octavian.preda@gmail.com>
2019-11-25 22:51:14 +0200

    Update return_statement.hpp

--- AST/statement/return_statement.hpp
+++ AST/statement/return_statement.hpp
@@ -1,11 +1,12 @@
+#include <memory>
 #include "../base/base_expression.hpp"
 #include "../base/base_statement.hpp"
 
 class ReturnStatement : public BaseStatement {
  private:
-  BaseExpression* expression;
+  std::shared_ptr<BaseExpression> expression;
 
  public:
-  ReturnStatement(BaseExpression* _expression) : expression(_expression) {}
-  //~ReturnStatement() { delete expression; }
+  ReturnStatement(std::shared_ptr<BaseExpression> _expression)
+      : expression(_expression) {}
 };
